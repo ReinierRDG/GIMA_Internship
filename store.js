65528b85e7ff262973b81368f30d30962247e515
ReinierRDG <49274394+ReinierRDG@users.noreply.github.com>
2019-08-23 20:31:36 +0200

    Add files via upload

--- store.js
+++ store.js
@@ -5,38 +5,21 @@ import Axios from "axios";
 Vue.use(Vuex);
 Vue.prototype.$http = Axios;
 
-const RESULTS_PER_PAGE = 18;
+const RESULTS_PER_PAGE = 20;
 
 export const store = new Vuex.Store({
-  // strict mode off to prevent mutation errors in the console.
   state: {
     form: {
-      keyword: "",
-      name: "",
-      location: "",
-      beginDate: null,
-      endDate: null
+      keyword: ""
     },
-    alpha: "",
     items: [],
-    curPage: 1,
     resultsCount: 0,
     fetchError: null,
-    toggledListItem: null,
-    selectedListItem: null,
-    activeListItem: null,
-    selection: null
+    curPage: 1,
+    activeDataset: [],
+    toggledListItem: null
   },
   mutations: {
-    changedSelection(state, selection) {
-      state.selection = selection;
-    },
-    toggleActiveListItem(state, toggledListItem) {
-      state.toggledListItem = toggledListItem;
-    },
-    changeActiveListItem(state, activeListItem) {
-      state.activeListItem = activeListItem;
-    },
     changedForm(state, form) {
       state.form = form;
       this.dispatch("fetchData");
@@ -44,36 +27,48 @@ export const store = new Vuex.Store({
     changedItems(state, items) {
       state.items = items;
     },
-    changedOrder(state, alpha) {
-      state.alpha = alpha;
-    },
     changedResultsCount(state, resultsCount) {
       state.resultsCount = resultsCount;
     },
     changedPage(state, curPage) {
       state.curPage = curPage;
       this.dispatch("fetchData");
+    },
+    changeActiveDataset(state, activeDataset) {
+      state.activeDataset = activeDataset;
+    },
+    toggleActiveListItem(state, toggledListItem) {
+      state.toggledListItem = toggledListItem;
+    },
+    submitChanges(state, activeDataset) {
+      state.activeDataset = activeDataset;
+      this.dispatch("patchEdit");
+    },
+    editedDataset(state, activeDataset) {
+      state.activeDataset = activeDataset;
+      console.log(activeDataset);
     }
   },
   actions: {
     fetchData(context) {
       Axios({
-        // use orions at work & luciad-dev.imagem.nl:8083 at home
-        method: "get",
+        // the request with method, url, authentication and headers
+        method: "GET",
         url: `http://orions/erdas-apollo/content/catalog/items?profile=full&keywords=${
           context.state.form.keyword
         }&start=${
           context.getters.start
         }&maxresults=${RESULTS_PER_PAGE}&classes=com.erdas.rsp.babel.model.GenericItem,%20com.erdas.rsp.babel.model.imagery.ImageReference,com.erdas.rsp.babel.model.vector.VectorReference,%20com.erdas.rsp.babel.model.pointcloud.PointCloudResource,%20com.erdas.rsp.babel.model.video.VideoResource`,
         auth: {
-          username: "xxxx",
-          password: "xxxx"
+          username: "admin",
+          password: "apollo123"
         },
         headers: {
           "Content-Type": "application/json",
           Accept: "*/*"
         }
       })
+        // response of the request creates a list of items, and counts the amount of results
         .then(response => {
           context.commit("changedItems", response.data.results);
           context.commit(
@@ -82,27 +77,71 @@ export const store = new Vuex.Store({
           );
           console.log(context.getters.items);
         })
+        // in case of no response: throw an error.
         .catch(error => {
           this.fetchError = error;
           console.log("No matches found", error);
         });
+    },
+    patchEdit(context) {
+      const activeDataset = context.state.activeDataset;
+      // let tags = activeDataset.tags.split(",");
+      Axios({
+        method: "patch",
+        proxyurl: "https://cors-anywhere.herokuapp.com/", // solves the boolean issues.. kind of..
+        url: `http://orions/erdas-apollo/content/catalog/items/${
+          activeDataset.id
+        }`,
+        auth: {
+          username: "admin",
+          password: "apollo123"
+        },
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "*/*"
+        },
+        data: {
+          // most properties seem to be protected from editing. Formatting/permissions in Apollo?
+          // tags: tags, // can be added by for example "tags": ["Brielle", "Zeeland"] as many as you want.!
+          // only getting to work by creating one array of values.. needs to be 1 array containing subs for each entry
+          name: activeDataset.name,
+          ogcUniqueName: activeDataset.ogcUniqueName,
+          title: activeDataset.title,
+          description: activeDataset.description,
+          acquisitionInfo: {
+            acquisitionDate: activeDataset.acquisitionInfo.acquisitionDate, // yy-mm-dd T hh:mm:ss Z, if only typing a year mm-dd will adapt to current day of edit. Any year goes.. needs validation
+            metadataAcquired: activeDataset.acquisitionInfo.metadataAcquired, // allows true or false
+            dataAcquired: activeDataset.acquisitionInfo.dataAcquired, // allows true or false
+            dataProcessed: activeDataset.acquisitionInfo.dataProcessed, // allows true or false
+            errorValue: activeDataset.acquisitionInfo.errorValue // Any integer, but I have no idea what the values stand for..
+          }
+        },
+        withCredentials: true,
+        crossDomain: true
+      })
+        .then(response => {
+          context.commit("editedDataset", response.data.results);
+          console.log(activeDataset);
+          alert("Wijzigingen opgeslagen");
+        })
+        .catch(error => {
+          this.fetchError = error;
+          console.log("No changes found", error);
+        });
     }
   },
   getters: {
-    form: state => {
-      return state.form;
-    },
     items: state => {
       return state.items;
-    },
-    start: state => {
-      return (state.curPage - 1) * RESULTS_PER_PAGE;
     },
     numPages: state => {
       return Math.ceil(state.resultsCount / RESULTS_PER_PAGE);
     },
-    resultsCount: state => {
-      return state.resultsCount;
+    start: state => {
+      return (state.curPage - 1) * RESULTS_PER_PAGE;
+    },
+    activeDataset: state => {
+      return state.activeDataset;
     }
   },
   watch: {
@@ -114,3 +153,16 @@ export const store = new Vuex.Store({
     }
   }
 });
+
+// Patch URL:
+// Use something different then patch? PUT? POST? Seems to be no put option in Apollo API.
+// Patch seems to be not supported by the server at this moment --> Will result in unending console errors (CORS related etc.)
+// Function should be called like this:
+// 1. Admin saves his changes,
+// 2. Save button will fill in the state of each property.
+// 3. Method should read the new state of the property and change this.
+// 4. New search should show updated values.
+// Workflow largely the same as the get (used before)
+
+// change input --> change active status --> update state --> patch ?
+// same authorization, same headers, different method, different response
